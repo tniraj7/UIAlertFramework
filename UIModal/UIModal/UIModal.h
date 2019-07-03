b1e85b1a452342e18976d7b9305c31165bb5e70e
Niraj Tiwari <niraj.tiwari@rakuten.com>
2019-07-03 17:10:56 +0900

    * Initialize project * Add xib file for modal view * Add class for modal view

---
+++ UIModal/UIModal/UIModal.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIModal.
+FOUNDATION_EXPORT double UIModalVersionNumber;
+
+//! Project version string for UIModal.
+FOUNDATION_EXPORT const unsigned char UIModalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIModal/PublicHeader.h>
+
+
