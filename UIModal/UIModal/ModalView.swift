00679049b89dfa78b7d7f53b6ddb78461f143fa1
Niraj Tiwari <tniraj7@gmail.com>
2019-07-04 18:52:49 +0900

    Add outlets from xib to class

--- UIModal/UIModal/ModalView.swift
+++ UIModal/UIModal/ModalView.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class ModalView: UIView {
 
+    @IBOutlet private weak var statusImage: UIImageView!
+    @IBOutlet private weak var headingLabel: UILabel!
+    @IBOutlet private weak var subheadingLabel: UILabel!
+    
 }
