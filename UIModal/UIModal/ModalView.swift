7020ebcab3fa3fafcac4463471ff0e7d9416ec60
Niraj Tiwari <tniraj7@gmail.com>
2019-07-05 21:15:13 +0900

    * add initializer for ModalView * add setup function

--- UIModal/UIModal/ModalView.swift
+++ UIModal/UIModal/ModalView.swift
@@ -6,4 +6,21 @@ class ModalView: UIView {
     @IBOutlet private weak var headingLabel: UILabel!
     @IBOutlet private weak var subheadingLabel: UILabel!
     
+    // MARK: Setup View
+    
+    let nibName = "ModalView"
+    var contentView: UIView!
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    func setUpView() {
+        
+    }
 }
