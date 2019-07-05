46f7ff58eb46a861049c69260ed6f192b3957ce1
Niraj Tiwari <tniraj7@gmail.com>
2019-07-05 21:38:39 +0900

    * Remove white spaces

--- UIModal/UIModal/ModalView.swift
+++ UIModal/UIModal/ModalView.swift
@@ -5,32 +5,33 @@ class ModalView: UIView {
     @IBOutlet private weak var statusImage: UIImageView!
     @IBOutlet private weak var headingLabel: UILabel!
     @IBOutlet private weak var subheadingLabel: UILabel!
-    
+
     // MARK: Setup View
-    
+
     let nibName = "ModalView"
     var contentView: UIView!
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         setUpView()
     }
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setUpView()
     }
-    
+
     func setUpView() {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: nibName, bundle: bundle)
 
         self.contentView = nib.instantiate(withOwner: self, options: nil).first as? UIView
         addSubview(contentView)
-        
+
         contentView.center = self.center
         contentView.autoresizingMask = []
         contentView.translatesAutoresizingMaskIntoConstraints = true
-        
+
         headingLabel.text = ""
         subheadingLabel.text = ""
     }
